314eca4cd31499f7ea086d5bf237e3b1b32a7b1d
Andrey <eckos997@gmail.com>
2020-09-16 21:40:14 +0500

    Mobile header

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,24 @@
-console.log(`yep`);
+const menuButton = document.querySelector(`.menu-btn`);
+
+const handleMenuClick = (clickEvent) => {
+    clickEvent.preventDefault();
+
+    const headerNavWrap = document.querySelector(`.header__nav-wrap`);
+    const headerWrap = document.querySelector(`.header__wrap`);
+
+    if (clickEvent.target.classList.contains(`menu-btn--closed`)) {
+        clickEvent.target.classList.remove(`menu-btn--closed`);
+        clickEvent.target.classList.add(`menu-btn--opened`);
+
+        headerNavWrap.classList.add(`header__nav-wrap--opened`);
+        headerWrap.classList.add(`header__wrap--opened`);
+    } else {
+        clickEvent.target.classList.remove(`menu-btn--opened`);
+        clickEvent.target.classList.add(`menu-btn--closed`);
+
+        headerNavWrap.classList.remove(`header__nav-wrap--opened`);
+        headerWrap.classList.remove(`header__wrap--opened`);
+    }
+}
+
+menuButton.addEventListener(`click`, handleMenuClick);
